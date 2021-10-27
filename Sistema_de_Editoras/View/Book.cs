749faff29a533b28f630071943b2bdc41b438465
Rafael Coimbra <rcoimbra1612@outlook.com>
2021-10-27 01:59:36 -0300

    001 - Atualização no Codígo

--- Sistema_de_Editoras/View/Book.cs
+++ Sistema_de_Editoras/View/Book.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sistema_de_Editoras.Controller;
+using Sistema_de_Editoras.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,7 @@ namespace Sistema_de_Editoras.View
     {
         public override void Criar()
         {
+            BookController books = new BookController();
             int opc = 0;
             do
             {
@@ -24,7 +27,15 @@ namespace Sistema_de_Editoras.View
 
                 if (opc == 20)
                 {
-                    
+                    BookModel bookk = new BookModel();
+                    bookk.CodLivro = (Int32.Parse(Console.ReadLine()));
+                    bookk.nameBook = (Console.ReadLine());
+                    bookk.livanoPublicação = (Int32.Parse(Console.ReadLine()));
+                    bookk.isbnBook = (decimal.Parse(Console.ReadLine()));
+
+                    books.Adicionar(bookk);
+
+                    Console.WriteLine("Livro Adicionado com sucesso!");
                 }
                 else if (opc == 21)
                 {
@@ -36,7 +47,19 @@ namespace Sistema_de_Editoras.View
                 }
                 else if (opc == 23)
                 {
+                    Int32 CodLivro = Int32.Parse(Console.ReadLine());
 
+                    BookModel searchBook = new BookModel();
+                    searchBook = books.search(CodLivro);
+
+                    if (searchBook == null)
+                    {
+                        Console.WriteLine("Codigo não localizado!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(searchBook.nameBook.ToString());
+                    }
                 }
             } while (opc != 29);
         }
